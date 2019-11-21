c453f2e5a584412edfabe539c6293f292a52ad23
JunZheng007 <i996zhjun@gmail.com>
2019-11-21 00:50:19 -0500

    update link

--- proj2_menu.php
+++ proj2_menu.php
@@ -5,11 +5,11 @@
 </head>
 <body>
     <h1>CSC33600 Project 2</h1>
-    <h2><a href="http://www-cs.ccny.cuny.edu/~zhen1363/proj2/add_producer.php">Add Producer</a></h2>
-    <h2><a href="http://www-cs.ccny.cuny.edu/~zhen1363/proj2/add_cd.php">Add CD</a></h2>
-    <h2><a href="http://www-cs.ccny.cuny.edu/~zhen1363/proj2/borrow_cd.php">Borrow CD</a></h2>
-    <h2><a href="http://www-cs.ccny.cuny.edu/~zhen1363/proj2/find_customer.php">Find Customer</a></h2>
-    <h2><a href="http://www-cs.ccny.cuny.edu/~zhen1363/proj2/find_producer.php">Find Producer</a></h2>
+    <h2><a href="http://www-cs.ccny.cuny.edu/~zhen1363/csc336-proj2/add_producer.php">Add Producer</a></h2>
+    <h2><a href="http://www-cs.ccny.cuny.edu/~zhen1363/csc336-proj2/add_cd.php">Add CD</a></h2>
+    <h2><a href="http://www-cs.ccny.cuny.edu/~zhen1363/csc336-proj2/borrow_cd.php">Borrow CD</a></h2>
+    <h2><a href="http://www-cs.ccny.cuny.edu/~zhen1363/csc336-proj2/find_customer.php">Find Customer</a></h2>
+    <h2><a href="http://www-cs.ccny.cuny.edu/~zhen1363/csc336-proj2/find_producer.php">Find Producer</a></h2>
 </body>
 
 </html>
